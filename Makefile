26aedd282a397b0119d25ae108cd02d1d0f4c7b8
matheusDall <matheus.dalmasomariano@gmail.com>
2024-08-20 14:43:31 -0300

    vah

---
+++ Makefile
@@ -0,0 +1,11 @@
+git:
+	git config --global user.email "matheus.dalmasomariano@gmail.com"
+	git config --global user.name "matheusDall"
+
+hello:
+		echo "hello world"
+
+build-java:
+
+run java: Build-java
+	java -cp bin Main
---
+++ trab.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class trab {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe um indereço IP:");
+        String ip = input.nextLine();
+
+
+    
+    }
+}
